adab7a428ff3d89d2ad1807bbe8b7860b39d05e5
Tomas <tomasespin12@gmail.com>
2025-09-18 12:47:18 -0300

    update deprecated lsp config & format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,123 +1,122 @@
 return {
-	-- Autocomplete
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local lsp_kind = require("lspkind")
+  -- Autocomplete
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local lsp_kind = require("lspkind")
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
 
-				mapping = cmp.mapping.preset.insert({
-					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-					["<C-o>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-o>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
 
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "buffer" },
-					{ name = "luasnip" },
-				}),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "luasnip" },
+        }),
 
-				formatting = {
-					format = lsp_kind.cmp_format({
-						mode = "symbol_text",
-						maxwidth = 50,
-					}),
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig", -- Nvim LSP client config
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			-- Capabilites -> "Useful for broadcasting to the server additional functionality (snippets, off-protocol features) provided by plugins."
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        formatting = {
+          format = lsp_kind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+          }),
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig", -- Nvim LSP client config
+    lazy = false,
+    config = function()
+      -- Capabilites -> "Useful for broadcasting to the server additional functionality (snippets, off-protocol features) provided by plugins."
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- Typescript / Javascript
-			lspconfig.ts_ls.setup({
-				filetypes = {
-					"javascript",
-					"javascriptreact",
-					"javascript.jsx",
-					"typescript",
-					"typescriptreact",
-					"typescript.tsx",
-				},
+      -- Typescript / Javascript
+      vim.lsp.config('ts_ls', {
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
 
-				init_options = {
-					hostInfo = "neovim",
-				},
+        init_options = {
+          hostInfo = "neovim",
+        },
 
-				capabilities = capabilities,
-			})
+        capabilities = capabilities,
+      })
+      vim.lsp.enable('ts_ls')
 
       -- eslint
-      lspconfig.eslint.setup({
+      vim.lsp.config('eslint', {
         capabilities = capabilities
       })
+      vim.lsp.enable('eslint')
 
-			-- Rubocop
-			-- lspconfig.rubocop.setup({
-			-- 	filetypes = { "ruby" },
-			--
-			-- 	capabilities = capabilities,
-			-- })
+      -- Ruby-LSP
+      vim.lsp.config('ruby_lsp', {
+        filetypes = { "ruby" },
 
-			-- Ruby-LSP
-			lspconfig.ruby_lsp.setup({
-				filetypes = { "ruby" },
+        capabilities = capabilities,
 
-				capabilities = capabilities,
+        cmd = { "/home/tomas1646/.rbenv/shims/ruby-lsp" },
+      })
+      vim.lsp.enable('ruby_lsp')
 
-				cmd = { "/home/tomas1646/.rbenv/shims/ruby-lsp" },
-			})
+      -- CSS
+      vim.lsp.config('cssls', {
+        filetypes = { "css", "scss", "less" },
 
-			-- CSS
-			lspconfig.cssls.setup({
-				filetypes = { "css", "scss", "less" },
+        capabilities = capabilities,
+      })
+      vim.lsp.enable('cssls')
 
-				capabilities = capabilities,
-			})
+      -- Lua
+      vim.lsp.config('lua_ls', {
+        filetypes = { "lua" },
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+        capabilities = capabilities,
+      })
+      vim.lsp.enable('lua_ls')
 
-			-- Lua
-			lspconfig.lua_ls.setup({
-				filetypes = { "lua" },
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
-				},
-				capabilities = capabilities,
-			})
+      -- C & C++ | Clangd
+      vim.lsp.config('clangd', {
+        filetypes = { "c", "cpp" },
 
-			-- C & C++ | Clangd
-			lspconfig.clangd.setup({
-				filetypes = { "c", "cpp" },
+        capabilities = capabilities,
+      })
+      vim.lsp.enable('clangd')
 
-				capabilities = capabilities,
-			})
-
-			-- Dockerfile
-			lspconfig.dockerls.setup({})
-		end,
-	},
+      -- Dockerfile
+      vim.lsp.config('dockerls', {})
+      vim.lsp.enable('dockerls')
+    end,
+  },
 }
